f3039352621bf761379c333cd155991b9737bd41
jean.laneurit <jean.laneurit@irstea.fr>
2020-09-29 09:26:31 +0200

    add conversion FSM -> string add conversion FSM -> DiagnosticStatus

--- include/romea_localisation/LocalisationFSMState.hpp
+++ include/romea_localisation/LocalisationFSMState.hpp
@@ -1,6 +1,9 @@
 #ifndef romea_LocalisationFSMState_hpp
 #define romea_LocalisationFSMState_hpp
 
+#include <string>
+#include <romea_common/diagnostic/DiagnosticStatus.hpp>
+
 namespace romea
 {
 
@@ -11,6 +14,10 @@ enum class LocalisationFSMState
   RESET,
   ABORTED
 };
+
+std::string toString(const LocalisationFSMState & fmsState);
+
+DiagnosticStatus toDiagnosticStatus(const LocalisationFSMState & fmsState);
 
 }
 
---
+++ src/LocalisationFSMState.cpp
@@ -0,0 +1,43 @@
+#include "romea_localisation/LocalisationFSMState.hpp"
+
+
+namespace romea
+{
+
+//-----------------------------------------------------------------------------
+std::string toString(const LocalisationFSMState & fmsState)
+{
+  switch(fmsState)
+  {
+  case LocalisationFSMState::INIT:
+    return "INIT";
+  case LocalisationFSMState::RUNNING:
+    return "RUNNING";
+  case LocalisationFSMState::RESET:
+    return "RESET";
+  case LocalisationFSMState::ABORTED:
+    return "ABORTED";
+  default:
+    return "";
+  }
+}
+
+//-----------------------------------------------------------------------------
+DiagnosticStatus toDiagnosticStatus(const LocalisationFSMState & fmsState)
+{
+  switch(fmsState)
+  {
+  case LocalisationFSMState::INIT:
+    return DiagnosticStatus::WARN;
+  case LocalisationFSMState::RUNNING:
+    return DiagnosticStatus::OK;
+  case LocalisationFSMState::RESET:
+    return DiagnosticStatus::WARN;
+  case LocalisationFSMState::ABORTED:
+    return DiagnosticStatus::ERROR;
+  default:
+    return DiagnosticStatus::STALE;
+  }
+}
+
+}
