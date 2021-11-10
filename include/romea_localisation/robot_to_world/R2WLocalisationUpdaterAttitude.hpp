cd9cccff60eaa7f173e2ba0a518a4182c56d4e4b
jean.laneurit <jean.laneurit@inrae.fr>
2021-11-10 11:50:51 +0100

    R2WLocalisationUpdaterAttitude : add test to ensure roll and pitch has the same variance

--- include/romea_localisation/robot_to_world/R2WLocalisationUpdaterAttitude.hpp
+++ include/romea_localisation/robot_to_world/R2WLocalisationUpdaterAttitude.hpp
@@ -3,8 +3,10 @@
 
 //romea
 #include <romea_common/time/Time.hpp>
+#include <romea_common/math/Algorithm.hpp>
 #include "../ObservationAttitude.hpp"
 #include "../LocalisationFSMState.hpp"
+
 
 namespace romea {
 
@@ -26,6 +28,7 @@ public :
               LocalisationFSMState & /*currentFSMState*/,
               MetaState &currentMetaState)
   {
+    assert(near(currentObservation.R(0,0),currentObservation.R(1,1)));
     currentMetaState.addon.roll=currentObservation.Y(ObservationAttitude::ROLL);
     currentMetaState.addon.pitch=currentObservation.Y(ObservationAttitude::PITCH);
     currentMetaState.addon.rollPitchVariance=currentObservation.R(ObservationAttitude::ROLL,
