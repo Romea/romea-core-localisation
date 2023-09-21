24e587d29390a082c0b223710ac0d778e805e1f9
Jean Laneurit <jean.laneurit@inrae.fr>
2023-09-21 16:16:23 +0200

    add support of linear speed, linear_speeds and anfular_speed updaters in R2R localisation traits
    Signed-off-by: Jean Laneurit <jean.laneurit@inrae.fr>

--- include/romea_core_localisation/robot_to_robot/R2RLocalisationTraits.hpp
+++ include/romea_core_localisation/robot_to_robot/R2RLocalisationTraits.hpp
@@ -6,6 +6,9 @@
 #include <romea_core_filtering/kalman/KalmanFilter.hpp>
 #include <romea_core_filtering/particle/ParticleFilter.hpp>
 #include "romea_core_localisation/LocalisationUpdaterTwist.hpp"
+#include "romea_core_localisation/LocalisationUpdaterLinearSpeed.hpp"
+#include "romea_core_localisation/LocalisationUpdaterLinearSpeeds.hpp"
+#include "romea_core_localisation/LocalisationUpdaterAngularSpeed.hpp"
 #include "romea_core_localisation/robot_to_robot/R2RLocalisationUpdaterLeaderTwist.hpp"
 #include "romea_core_localisation/robot_to_robot/kalman/R2RLocalisationKFResults.hpp"
 #include "romea_core_localisation/robot_to_robot/kalman/R2RLocalisationKFUpdaterRange.hpp"
@@ -31,8 +34,11 @@ template<>
 struct R2RLocalisationTraits<KALMAN>
 {
   using Filter = KalmanFilter<R2RLocalisationKFMetaState, LocalisationFSMState, Duration>;
-  using UpdaterLeaderTwist = R2RLocalisationUpdaterLeaderTwist<R2RLocalisationKFMetaState>;
   using UpdaterTwist = LocalisationUpdaterTwist<R2RLocalisationKFMetaState>;
+  using UpdaterLeaderTwist = R2RLocalisationUpdaterLeaderTwist<R2RLocalisationKFMetaState>;
+  using UpdaterLinearSpeed = LocalisationUpdaterLinearSpeed<R2RLocalisationKFMetaState>;
+  using UpdaterLinearSpeeds = LocalisationUpdaterLinearSpeeds<R2RLocalisationKFMetaState>;
+  using UpdaterAngularSpeed = LocalisationUpdaterAngularSpeed<R2RLocalisationKFMetaState>;
   using UpdaterPose = R2RLocalisationKFUpdaterLeaderPose;
   using UpdaterRange = R2RLocalisationKFUpdaterRange;
   using Predictor = R2RLocalisationKFPredictor;
@@ -43,8 +49,11 @@ template<>
 struct R2RLocalisationTraits<PARTICLE>
 {
   using Filter = ParticleFilter<R2RLocalisationPFMetaState, LocalisationFSMState, Duration>;
-  using UpdaterLeaderTwist = R2RLocalisationUpdaterLeaderTwist<R2RLocalisationPFMetaState>;
   using UpdaterTwist = LocalisationUpdaterTwist<R2RLocalisationPFMetaState>;
+  using UpdaterLeaderTwist = R2RLocalisationUpdaterLeaderTwist<R2RLocalisationPFMetaState>;
+  using UpdaterLinearSpeed = LocalisationUpdaterLinearSpeed<R2RLocalisationPFMetaState>;
+  using UpdaterLinearSpeeds = LocalisationUpdaterLinearSpeeds<R2RLocalisationPFMetaState>;
+  using UpdaterAngularSpeed = LocalisationUpdaterAngularSpeed<R2RLocalisationPFMetaState>;
   using UpdaterPose = R2RLocalisationPFUpdaterLeaderPose;
   using UpdaterRange = R2RLocalisationPFUpdaterRange;
   using Predictor = R2RLocalisationPFPredictor;
